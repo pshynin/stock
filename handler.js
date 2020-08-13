75ede639d2f32d6c4d5b1657b614dc9c656d359c
Paul Shynin <paul.shynin@onepeloton.com>
2020-08-13 08:23:46 -0400

    dummy finction and dummy event

--- handler.js
+++ handler.js
@@ -12,7 +12,8 @@ module.exports.hello = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.stock = (evt, ctx, done) => {
+    return { message: evt };
+}
